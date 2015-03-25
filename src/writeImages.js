aaaf4c27fa3cf1c3657d7d471229f2e5048e6bf9
Jim McAndrew <jim@loc8.us>
2015-03-25 14:11:27 -0600

    Library to loop through images

---
+++ src/writeImages.js
@@ -0,0 +1,28 @@
+var Bluebird = require('bluebird'),
+  datawrap = require('datawrap'),
+  fs = require('fs'),
+  runList = datawrap.runList,
+  path = require('path'),
+  writeToGithub = require('./writeToGithub');
+
+module.exports = function(imageList, githubSettings) {
+  return new Bluebird(function(fulfill, reject) {
+    console.log('w0');
+    var taskList = [];
+    imageList.map(function(img) {
+      if (fs.existsSync(img.o)) {
+        taskList.push({
+          'name': 'Uploading ' + img.o + ' to github',
+          'task': writeToGithub,
+          'params': [img.o, path.basename(img.o), githubSettings]
+        });
+      } else {
+        reject('File ' + img.o + ' not found');
+      }
+    });
+
+    runList(taskList, 'Upload to GitHub')
+      .then(fulfill)
+      .catch(reject);
+  });
+};
