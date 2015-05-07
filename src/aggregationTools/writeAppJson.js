0a19fcf622fbc1c6d0c44c6631a1b670aa38491a
Jim McAndrew <jim@loc8.us>
2015-05-07 17:23:24 -0600

    Add new tast

--- src/aggregationTools/writeAppJson.js
+++ src/aggregationTools/writeAppJson.js
@@ -1,1 +1,21 @@
-module.exports = function(options, unitCode, AppJson){};
+var Bluebird = require('bluebird');
+var writeData = require('../github/writeData');
+
+module.exports = function(appJson, unitCode, config) {
+  return new Bluebird(function(fulfill, reject) {
+    var githubSettings = JSON.parse(JSON.stringify(config.github));
+    var githubPath = 'places_mobile/' + unitCode + '/app.json';
+    writeData(JSON.stringify(appJson, null, 2), githubPath, githubSettings, config)
+      .then(function(r) {
+        delete githubSettings.filePath;
+        var newGithubPath = githubPath.replace(/\.json$/, '.min.json');
+        config.github.filePath = newGithubPath;
+        writeData(JSON.stringify(appJson), newGithubPath, config.github, config)
+          .then(function(r2) {
+            fulfill([r, r2]);
+          })
+          .catch(reject);
+      })
+      .catch(reject);
+  });
+};
