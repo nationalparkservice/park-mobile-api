1f6e07b1afe3f6ff7fcbe8c994d796fc269c3764
Jim McAndrew <jim@loc8.us>
2015-05-08 12:45:33 -0600

    Create tasklist for thumbnails

--- src/aggregationTools/writeThumbnails.js
+++ src/aggregationTools/writeThumbnails.js
@@ -1,6 +1,121 @@
 var Bluebird = require('bluebird');
+var datawrap = require('datawrap');
+var thumbnailSettings = require('../../thumbnailSettings');
+var magickTypes = require('../../node_modules/magick-resize/types');
 var writeData = require('../github/writeData');
+var geoTools = require('../geoTools.js');
+var magickResize = require('magick-resize');
 
+var addOffset = function(obj) {
+  obj.longitudeMarker = obj.zoom && obj.markerOffsetX ? geoTools.addPixelsToLong(obj.markerOffsetX, obj.longitude, obj.zoom).toString() : obj.longitude;
+  obj.latitudeMarker = obj.zoom && obj.markerOffsetY ? geoTools.addPixelsToLat(obj.markerOffsetY, obj.latitude, obj.zoom).toString() : obj.latitude;
+  obj.longitudeImage = obj.zoom && obj.offsetX ? geoTools.addPixelsToLong(obj.offsetX, obj.longitude, obj.zoom).toString() : obj.longitude;
+  obj.latitudeImage = obj.zoom && obj.offsetY ? geoTools.addPixelsToLat(obj.offsetY, obj.latitude, obj.zoom).toString() : obj.latitude;
+  return obj;
+};
+var objMerge = function(objs) {
+  var newObj = {},
+    attrname;
+  for (var i = 0; i < objs.length; i++) {
+    for (attrname in objs[i]) {
+      if (objs[i][attrname]) newObj[attrname] = objs[i][attrname].toString();
+    }
+  }
+  newObj.icon = encodeURIComponent(newObj.icon);
+  return newObj;
+};
+
+var magickResizeWrapper = function(args) {
+  return new Bluebird(function(fulfill, reject) {
+    // magickResize takes single letter args since it is a command line tool, so we just use the first letter
+    var newArgs = {};
+    for (var arg in args) {
+      if (arg.substr(0, 1) !== '_') {
+        newArgs[arg.substr(0, 1)] = args[arg];
+      }
+    }
+    magickResize(newArgs, function(e, r) {
+      if (e || !r) {
+        reject(e.stack ? e : new Error(e));
+      } else {
+        fulfill(0);
+      }
+    });
+  });
+};
+
+var formatValue = function(value) {
+  var returnValue = null;
+  if (value !== false && value) {
+    if (isNaN(value)) {
+      returnValue = value;
+    } else {
+      returnValue = parseFloat(value, 10);
+    }
+  } else {
+    returnValue = value === false ? false : null;
+  }
+  return returnValue;
+};
+
+var getRequests = function(thumbnailList, unitCode, config) {
+  var requestList = [];
+  var settings = thumbnailSettings[unitCode] || thumbnailSettings['default'];
+  thumbnailList.map(function(img) {
+    settings.map(function(setting) {
+      var imgRequest = {
+        // _img: img,
+        type: setting.type,
+        output: (formatValue(img[setting.field]) ? Math.random().toString(27).substr(2,10) + '_' + formatValue(img[setting.field]) : null)
+      };
+      if (img[setting.field]) {
+        // We are making an image for this setting!
+        imgRequest._filename = img[setting.field];
+        imgRequest.url = datawrap.fandlebars(setting.url, addOffset(objMerge([setting, magickTypes[setting.type], img, {
+          'token': config.mapbox.token
+        }])));
+      requestList.push(imgRequest);
+      }
+    });
+  });
+  return requestList;
+};
+
+var getThumbnailData = function(media, sites) {
+  var checkField = function(parents, field) {
+    return parents && parents[field] ? parents[field] : null;
+  };
+
+  // Filter out the array so we only have thumbnail
+  var thumbnails = media.filter(function(a) {
+    return a.type === 'map_thumbnail';
+  });
+
+  var findMatch = function(sites, thumbnailId) {
+    for (var i = 0; i < sites.length; i++) {
+      if (checkField(sites[i], 'map_thumbnail_image') === thumbnailId) {
+        return i;
+      }
+    }
+    return null;
+  };
+
+  // Add the lat and lon to the thumbnail
+  thumbnails = thumbnails.map(function(thumbnail) {
+    var match = findMatch(sites, thumbnail.id);
+    if (match) {
+      thumbnail.latitude = sites[match].latitude;
+      thumbnail.longitude = sites[match].longitude;
+    } else {}
+    return match ? thumbnail : null;
+  });
+
+  // Remove the unmatched thumbnails
+  thumbnails = thumbnails.filter(function(a) {
+    return !!a;
+  });
+  return thumbnails;
+};
 module.exports = function(config, unitCode, parkJson) {
   var media, sites;
   return new Bluebird(function(fulfill, reject) {
@@ -14,32 +129,9 @@ module.exports = function(config, unitCode, parkJson) {
       // If there are no sites, reject this!
       reject(e);
     }
+    var matchedThumbnails = getThumbnailData(media, sites);
+    var imgRequests = getRequests(matchedThumbnails, unitCode, config);
 
-    // Filter out the array so we only have thumbnail
-    var thumbnails = media.filter(function(a) {
-      return a.type === 'map_thumbnail';
-    });
-
-    var findMatch = function(sites, thumbnailId) {
-      for (var i = 0; i < sites.length; i++) {
-        if (sites[i].map_thumbnail_image === thumbnailId) {
-          return i;
-        }
-      }
-      return null;
-    };
-
-    // Add the lat and lon to the thumbnail
-    thumbnails = thumbnails.map(function(thumbnail) {
-      var match = findMatch(sites, thumbnail.id);
-      if (match) {
-        thumbnail.latitude = sites[match].latitude;
-        thumbnail.longitude = sites[match].longitude;
-      } else {
-      }
-      return match ? thumbnail : null;
-    });
-
-    fulfill(thumbnails);
+    fulfill(imgRequests);
   });
 };
---
+++ src/geoTools.js
@@ -0,0 +1,27 @@
+var geoTools = module.exports = {
+  addPixelsToLat: function(pixels, y, zoom) {
+    pixels = parseFloat(pixels, 10);
+    y = parseFloat(y, 10);
+    zoom = parseFloat(zoom, 10);
+    return geoTools.tile2lat(geoTools.lat2tile(y, zoom) - (pixels / 256), zoom);
+  },
+  addPixelsToLong: function(pixels, x, zoom) {
+    pixels = parseFloat(pixels, 10);
+    x = parseFloat(x, 10);
+    zoom = parseFloat(zoom, 10);
+    return geoTools.tile2long(geoTools.long2tile(x, zoom) - (pixels / 256), zoom);
+  },
+  lat2tile: function(lat, zoom) {
+    return ((1 - Math.log(Math.tan(lat * Math.PI / 180) + 1 / Math.cos(lat * Math.PI / 180)) / Math.PI) / 2 * Math.pow(2, zoom));
+  },
+  long2tile: function(lon, zoom) {
+    return ((lon + 180) / 360 * Math.pow(2, zoom));
+  },
+  tile2lat: function(y, z) {
+    var n = Math.PI - 2 * Math.PI * y / Math.pow(2, z);
+    return (180 / Math.PI * Math.atan(0.5 * (Math.exp(n) - Math.exp(-n))));
+  },
+  tile2long: function(x, z) {
+    return (x / Math.pow(2, z) * 360 - 180);
+  }
+};
