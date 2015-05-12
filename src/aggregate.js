d9665a15ddca41c2e9ec3512f9c6a98915681ca6
Jim McAndrew <jim@loc8.us>
2015-05-12 16:52:31 -0600

    Make it clear what this tool is doing

--- src/aggregate.js
+++ src/aggregate.js
@@ -1,70 +1,59 @@
-// This tool will read a schema and pull the schema information out of CartoDB
-// It takes the following options:
-// 'unitCode', //A unit code can be specified, if it is, only that park will be run
-// 'schema', // Path to the schema (allows us the use a legacy schema0
-// 'default', // ** REQUIRED ** Specify a file to use with default settings
-// 'resize' // Which images to resize, falsey means no images, 1 means all images, otherwise you need to specify which images you'd like resize (such as "map_thumbnails")
-
 var datawrap = require('datawrap');
-var moment = require('moment');
-
-// Get the datawrap tools
-var Bluebird = datawrap.Bluebird;
-var runList = datawrap.runList;
 
 // Require the external tools
-var getParkList = require('./aggregationTasks/getParkList'); // = function(unitCode){};
-var generateSchema = require('./aggregationTasks/generateSchema.js'); // = function(options, unitCode)
-var writeThumbnails = require('./aggregationTasks/writeThumbnails.js'); // = function(options, unitCode, AppJson)
-var writeAppJson = require('./aggregationTasks/writeAppJson.js'); // = function(options, unitCode, AppJson)
-var writeMetaJson = require('./aggregationTasks/writeMetaJson.js'); // = function(options, unitCode, MetaJson)
+var getParkList = require('./aggregationTools/getParkList'); // = function(unitCode(s)){}; - return array of unitCodes
+var generateData = require('./aggregationTools/generateData'); // = function(schemaPath, unitCode) -- returns SchemaJson and ParkJson
+var generateSchema = require('./aggregationTools/generateSchema'); // = function({SchemaJson, ParkJson}); -- Returns AppJson
+var writeThumbnails = require('./aggregationTools/writeThumbnails'); // = function(config, unitCode, AppJson)
+var writeAppJson = require('./aggregationTools/writeAppJson'); // = function(AppJson, unitCode, config)
+var writeMetaJson = require('./aggregationTools/writeMetaJson'); // = function(appJson, unitCode, config)
 
-// If no defaults are specified, use the defaults.json file
+var aggregatePark = function(schemaPath, unitCode, config) {
+  return new datawrap.Bluebird(function(fulfill, reject) {
+    var taskList = [{
+      'name': 'GenerateData',
+      'task': generateData,
+      'params': [schemaPath, unitCode, config]
+    }, {
+      'name': 'GenerateSchema',
+      'task': generateSchema,
+      'params': ['{{GenerateData}}']
+    }, {
+      'name': 'Write the thumbnails',
+      'task': writeThumbnails,
+      'params': ['{{GenerateSchema}}', unitCode, config]
+    }, {
+      'name': 'Generate Data from CartoDB',
+      'task': writeAppJson,
+      'params': ['{{GenerateSchema}}', unitCode, config]
+    }, {
+      'name': 'Generate Data from CartoDB',
+      'task': writeMetaJson,
+      'params': ['{{GenerateSchema}}', unitCode, config]
+    }];
 
+    datawrap.runList(taskList)
+      .then(fulfill)
+      .catch(reject);
+  });
+};
 
-// The first step is to query the "Parks" table in CartoDB to get a list of Parks
-//   SELECT DISTINCT unit_code FROM places_mobile_parks WHERE is_demo = false;
-module.exports = function(options) {
-  return new Bluebird(function(resolve, reject) {
-    getParkList(options.unitCode)
-      .catch(reject)
-      .then(function(parks) {
-        // Then we loop through the list of parks
-        var schemaTasks = parks.map(function(unitCode) {
+module.exports = function(schemaPath, unitCodes, config) {
+  return new datawrap.Bluebird(function(resolve, reject) {
+    getParkList(unitCodes)
+      .then(function(validParkList) {
+        var taskList = validParkList.map(function(unitCode) {
           return {
-            'name': 'Generating the App.json for ' + unitCode,
-            'task': generateSchema,
-            'params': [options, unitCode]
+            'name': 'Aggregate ' + unitCode,
+            'task': aggregatePark,
+            'params': [schemaPath, unitCode, config]
           };
         });
-        runList(schemaTasks, 'Generate App.json')
-          .catch(reject)
-          .then(function(results) {
-            var writeTasks = results.map(function(result) {
-              return [{
-                // Redraw thumbnails for sites
-                'name': 'Writing thumbnails for ' + result.unitCode,
-                'task': writeThumbnails,
-                'params': [options, result.unitCode, result.json]
-              }, {
-                // Write the app.json file to github
-                'name': 'Writing app.json for ' + result.unitCode,
-                'task': writeAppJson,
-                'params': [options, result.unitCode, result.json]
-              }, {
-                // Write the meta file to github
-                'name': 'Write meta.json for ' + result.unitCode,
-                'task': writeMetaJson,
-                'params': [options, result.unitCode, {
-                  'api_version': result.apiVersion,
-                  'last_data_update': moment().format()
-                }]
-              }];
-            });
-            runList(writeTasks, 'Writing the files to github')
-              .catch(reject)
-              .then(resolve);
-          });
-      });
+
+        datawrap.runList(taskList)
+          .then(resolve)
+          .catch(reject);
+      })
+      .catch(reject);
   });
 };
