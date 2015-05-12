e27c65f8fb2d13bcadb996d2373c83627c016082
Jim McAndrew <jim@loc8.us>
2015-05-12 16:54:31 -0600

    Update the generateSchema reference

--- src/aggregationTools/tests/writeThumbnails.js
+++ src/aggregationTools/tests/writeThumbnails.js
@@ -3,19 +3,18 @@ var generateSchema = require('../generateSchema');
 var writeThumbnails = require('../writeThumbnails');
 var config = require('../../../config');
 var schemaFile = '../../../app.schema.json';
+var unitCode = 'Brooklyn';
 
 console.log('Generating Data');
-generateData({
-    schema: schemaFile
-  }, 'klgo')
+generateData(schemaFile, unitCode, config)
   .then(function(r) {
     console.log('************* DATA COMPLETED ****************');
     console.log('Schema Data');
-    generateSchema(r.schema, r.parkData)
+    generateSchema(r)
       .then(function(r2) {
         console.log('************* SCHEMA COMPLETED ****************');
         console.log('Thumbnail Data');
-        writeThumbnails(config, 'klgo', r2).then(function(r3) {
+        writeThumbnails(r2, unitCode, config).then(function(r3) {
           console.log('************* Thumbnail COMPLETED ****************');
           console.log('Success: ', r3);
         }).catch(function(e) {
