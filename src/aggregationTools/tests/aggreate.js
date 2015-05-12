eccd01b17755ddeff376734b89dcedb542235805
Jim McAndrew <jim@loc8.us>
2015-05-12 16:51:41 -0600

    Add a test for the aggregation script

---
+++ src/aggregationTools/tests/aggreate.js
@@ -0,0 +1,12 @@
+var aggregate = require('../../aggregate');
+var config = require('../../../config');
+
+aggregate('../../../app.schema.json', ['Brooklyn'] , config)
+  .then(function(r) {
+    console.log(JSON.stringify(r, null, 2));
+    console.log('Success!');
+  })
+  .catch(function(e) {
+    console.log('Failure!');
+    throw (e);
+  });
