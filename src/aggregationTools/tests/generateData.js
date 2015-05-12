8a9808dbd8f40976bd65df963e3b547d396fd1fa
Jim McAndrew <jim@loc8.us>
2015-05-12 16:53:34 -0600

    Clean up for file instead of github usage

--- src/aggregationTools/tests/generateData.js
+++ src/aggregationTools/tests/generateData.js
@@ -1,9 +1,8 @@
 var generateData = require('../generateData');
 var schemaFile = '../../../app.schema.json';
+var config = require('../../../config');
 
-generateData({
-    schema: schemaFile
-  }, 'klgo')
+generateData(schemaFile, 'klgo', config)
   .then(function(r) {
     console.log('Success: ', JSON.stringify(r, null, 1));
   })
