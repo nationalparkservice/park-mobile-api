ea6230db02ddd152948d7909ecc658b5a35c387e
Jim McAndrew <jim@loc8.us>
2015-05-12 16:53:21 -0600

    Only return 'valid' parks

--- src/aggregationTools/getParkList.js
+++ src/aggregationTools/getParkList.js
@@ -9,17 +9,23 @@ var Bluebird = datawrap.Bluebird;
 module.exports = function(unitCode) {
   return new Bluebird(function(fulfill, reject) {
     var fieldName = 'unit_code',
-    parksQuery = 'SELECT DISTINCT {{fieldName}} FROM places_mobile_parks WHERE is_demo = false';
-    if (unitCode) {
-      fulfill(Array.isArray(unitCode) ? unitCode : [unitCode]);
-    } else {
-      database.runQuery(parksQuery, {fieldName: fieldName})
-        .catch(reject)
-        .then(function(result) {
-          fulfill(result[0].rows.map(function(row) {
-            return row[fieldName];
-          }));
+      parksQuery = 'SELECT DISTINCT {{fieldName}} FROM places_mobile_parks WHERE is_demo = false';
+    //fulfill(Array.isArray(unitCode) ? unitCode : [unitCode]);
+    database.runQuery(parksQuery, {
+        fieldName: fieldName
+      })
+      .then(function(result) {
+        var availableUnitCodes = result[0].rows.map(function(row) {
+          return row[fieldName];
         });
-    }
+        if (unitCode) {
+          unitCode = Array.isArray(unitCode) ? unitCode : [unitCode];
+          availableUnitCodes = availableUnitCodes.filter(function(uc) {
+            return unitCode.indexOf(uc) >= 0;
+          });
+        }
+        fulfill(availableUnitCodes);
+      })
+      .catch(reject);
   });
 };
