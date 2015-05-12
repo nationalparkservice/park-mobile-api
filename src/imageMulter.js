5c87acd06cb7dd895fd0bfa1e738b6f36734fcb8
Jim McAndrew <jim@loc8.us>
2015-05-12 16:55:21 -0600

    Remove error logging

--- src/imageMulter.js
+++ src/imageMulter.js
@@ -1,12 +1,8 @@
-var errorLog = require('./errorLog'),
-  multer = require('multer');
+var multer = require('multer');
 
 module.exports = multer({
   dest: './uploads/',
   rename: function(fieldname, filename) {
     return filename.replace(/\W+/g, ' - ').toLowerCase() + Date.now();
-  },
-  onFileUploadComplete: function(file) {
-    errorLog(file.originalname + 'uploaded to ' + file.path);
   }
 });
