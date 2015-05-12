18ce254a434dc55cbca9f14bedfd30afee43af95
Jim McAndrew <jim@loc8.us>
2015-05-12 16:54:56 -0600

    Use fs instead of github

--- src/aggregationTools/writeMetaJson.js
+++ src/aggregationTools/writeMetaJson.js
@@ -1,5 +1,9 @@
-var Bluebird = require('bluebird');
-var writeData = require('../github/writeData');
+/*jshint camelcase: false */
+var Bluebird = require('datawrap').Bluebird,
+  fs = require('fs'),
+  mkdirp = require('mkdirp'),
+  path = require('path');
+fs = Bluebird.promisifyAll(fs);
 
 module.exports = function(appJson, unitCode, config) {
   var date = new Date(),
@@ -7,20 +11,24 @@ module.exports = function(appJson, unitCode, config) {
       'api_version': appJson.api_version,
       'last_data_update': date.toISOString()
     };
+
   return new Bluebird(function(fulfill, reject) {
-    var githubSettings = JSON.parse(JSON.stringify(config.github));
-    var githubPath = 'places_mobile/' + unitCode + '/meta.json';
-    writeData(JSON.stringify(meta, null, 2), githubPath, githubSettings, config)
-      .then(function(r) {
-        delete githubSettings.filePath;
-        var newGithubPath = githubPath.replace(/\.json$/, '.min.json');
-        config.github.filePath = newGithubPath;
-        writeData(JSON.stringify(meta), newGithubPath, config.github, config)
-          .then(function(r2) {
-            fulfill([r, r2]);
+    var filePath = config.fileLocation + '/' + unitCode + '/meta.json';
+    var minFilePath = filePath.replace(/\.json$/, '.min.json');
+    mkdirp(path.dirname(filePath), function(err) {
+      if (err) {
+        reject(err);
+      } else {
+        fs.writeFileAsync(filePath, JSON.stringify(meta, null, 2))
+          .then(function(r) {
+            fs.writeFileAsync(minFilePath, JSON.stringify(meta))
+              .then(function(r2) {
+                fulfill([r, r2]);
+              })
+              .catch(reject);
           })
           .catch(reject);
-      })
-      .catch(reject);
+      }
+    });
   });
 };
