8f20e4087ddb8a2671c4bc586530764ec6eacbb7
Jim McAndrew <jim@loc8.us>
2015-05-14 14:20:10 -0600

    Test the individual write and the multiple write functions of the thumbnail tool

--- src/aggregationTools/tests/writeThumbnails.js
+++ src/aggregationTools/tests/writeThumbnails.js
@@ -14,13 +14,31 @@ generateData(schemaFile, unitCode, config)
       .then(function(r2) {
         console.log('************* SCHEMA COMPLETED ****************');
         console.log('Thumbnail Data');
-        writeThumbnails(r2, unitCode, config).then(function(r3) {
-          console.log('************* Thumbnail COMPLETED ****************');
-          console.log('Success: ', r3);
-        }).catch(function(e) {
-          console.log('Thumbnail Error: ', e);
-          throw e;
-        });
+        writeThumbnails(r2, unitCode, config, true)
+          .then(function(r3) {
+            console.log('************* All Thumbnails COMPLETED ****************');
+            console.log('Success: ', r3);
+            writeThumbnails(r2, unitCode, config, 66)
+              .then(function(r4) {
+                console.log('************* Individual Thumbnails COMPLETED ****************');
+                console.log('Success: ', r4);
+                writeThumbnails(r2, unitCode, config, [64, 67])
+                  .then(function(r5) {
+                    console.log('************* Multiple Defined Thumbnails COMPLETED ****************');
+                    console.log('Success: ', r5);
+                  }).catch(function(e) {
+                    console.log('Individual Thumbnail Error: ', e);
+                    throw e;
+                  });
+              }).catch(function(e) {
+                console.log('Individual Thumbnail Error: ', e);
+                throw e;
+              });
+          })
+          .catch(function(e) {
+            console.log('All Thumbnail Error: ', e);
+            throw e;
+          });
       })
       .catch(function(e) {
         console.log('Schema Error: ', e);
