3a2b39b6384baf3b7f1bb022d159687b4342dfeb
Jim McAndrew <jim@loc8.us>
2015-05-27 16:30:20 -0600

    Allow for possibility of an error with the storage

--- src/getStatus.js
+++ src/getStatus.js
@@ -4,7 +4,11 @@ module.exports = function(req, res) {
   storage.initSync();
   if (req.params.process && storage.getItem(req.params.process)) {
     storage.getItem(req.params.process, function(err, value) {
-      res.send(value);
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(value);
+      }
     });
   } else {
     res.send('Process "' + req.params.process + '" not found');
