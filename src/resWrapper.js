ebad23c8c360e936a5758c745536ddfc9fe93445
Jim McAndrew <jim@loc8.us>
2015-05-28 14:51:38 -0600

    Remove extra logging: Update src/resWrapper.js

--- src/resWrapper.js
+++ src/resWrapper.js
@@ -10,13 +10,11 @@ module.exports = function(req, origRes) {
   var responseUuid = createUuid(),
     newRes = {
       'error': function(error) {
-        console.log('ERROR', error);
         return newRes.send(error, {
           status: 500
         });
       },
       'send': function(result, options) {
-        console.log('Sending', result, options);
         var buf, newResult = typeof result === 'object' ? JSON.stringify(result, null, 2) : result.toString(),
           status = options && options.status || '200',
           returnValue;
@@ -32,15 +30,11 @@ module.exports = function(req, origRes) {
             origRes.set('Content-Encoding', 'gzip');
             buf = new Buffer(newResult, 'utf-8');
             zlib.gzip(buf, function(err, zippedResult) {
-              console.log('send as gzip', contentTypes[typeof result] ? contentTypes[typeof result] : contentTypes.default);
               returnValue = origRes.end(zippedResult);
             });
           } else {
-            console.log('send as raw', contentTypes[typeof result] ? contentTypes[typeof result] : contentTypes.default);
             returnValue = origRes.send(newResult);
           }
-        } else {
-          console.log('Not sending, headers already sent!');
         }
         return returnValue; // Return the original result, just in case there's a need for that
       }
