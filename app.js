0f740405e412d124eb011235015b0adbce057f21
Jim McAndrew <jim@loc8.us>
2015-08-18 12:15:31 -0600

    move to semistandard

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
-var allowXSS = require('./src/allowXSS'),
-  Api = require('./src/api'),
-  config = require('./config'),
-  express = require('express'),
-  port = config.port || 3002;
+var Api = require('./src/api');
+var allowXSS = require('./src/allowXSS');
+var config = require('./config');
+var express = require('express');
+var port = config.port;
 
-var app = express(),
-  api = new Api(config),
-  apiRouter = express.Router();
-  allowXSS(apiRouter);
+var app = express();
+var api = new Api(config);
+var apiRouter = express.Router();
+allowXSS(apiRouter);
 
 config.htmlDirectory = __dirname + '/' + config.htmlDirectory;
 
-api.map(function(apiCall) {
+api.map(function (apiCall) {
   apiRouter[apiCall.method.toLowerCase()]('/api' + apiCall.path, apiCall.process);
 });
 
 app.use(apiRouter);
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log('Working on port ' + port);
 });
