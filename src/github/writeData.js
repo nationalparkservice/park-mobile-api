68cec10efb510adc2367748a63b13e581ddf1a4b
Jim McAndrew <jim@loc8.us>
2016-04-28 07:47:18 -0600

    Order the try/catch

--- src/github/writeData.js
+++ src/github/writeData.js
@@ -1,20 +1,20 @@
-var Bluebird = require('bluebird'),
-  githubFunctions = require('./githubFunctions');
+var Bluebird = require('bluebird');
+var githubFunctions = require('./githubFunctions');
 
-module.exports = function(fileData, githubFileName, githubSettings, config) {
+module.exports = function (fileData, githubFileName, githubSettings, config) {
   githubSettings.filePath = githubSettings.filePath || githubFileName;
-  return new Bluebird(function(fulfill, reject) {
+  return new Bluebird(function (fulfill, reject) {
     var requestOptions = githubFunctions.headers.main(githubSettings, config);
     githubFunctions.checkGithubFile(requestOptions)
-      .catch(reject)
-      .then(function(githubResponse) {
+      .then(function (githubResponse) {
         var message = githubResponse.statusCode === 404 ? 'Create' : 'Update';
         message += ' ' + requestOptions.url.split('/').slice(-3).join('/').split('?')[0] + ' with ' + config.appName;
         requestOptions.method = 'put';
         requestOptions.body = JSON.stringify(githubFunctions.headers.body(githubResponse.sha, githubSettings.branch, message, fileData));
         githubFunctions.updateGithubData(requestOptions)
-          .catch(reject)
-          .then(fulfill);
-      });
+          .then(fulfill)
+          .catch(reject);
+      })
+      .catch(reject);
   });
 };
