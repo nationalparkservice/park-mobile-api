7768f939d29b8762a2c36d2a4efb518a90e35124
Jim McAndrew <jim@loc8.us>
2016-05-06 14:32:28 -0600

    Check to see if the file was rewritten before writing a new meta file

--- src/aggregationTools/writeMetaJson.js
+++ src/aggregationTools/writeMetaJson.js
@@ -1,33 +1,35 @@
-/*jshint camelcase: false */
-var Bluebird = require('datawrap').Bluebird,
-  fs = require('fs'),
-  mkdirp = require('mkdirp'),
-  path = require('path');
-fs = Bluebird.promisifyAll(fs);
+var Promise = require('bluebird');
+var fs = require('fs');
+var mkdirp = require('mkdirp');
+var path = require('path');
+fs = Promise.promisifyAll(fs);
 
-module.exports = function(appJson, unitCode, config) {
-  var date = new Date(),
-    meta = {
-      'api_version': appJson.api_version,
-      'last_data_update': date.toISOString()
-    };
+module.exports = function (appJson, unitCode, config, appJsonWrites) {
+  var date = new Date();
+  var meta = {
+    'api_version': appJson.api_version,
+    'last_data_update': date.toISOString()
+  };
 
-  return new Bluebird(function(fulfill, reject) {
+  return new Promise(function (fulfill, reject) {
     var filePath = config.fileLocation + '/' + unitCode + '/meta.json';
     var minFilePath = filePath.replace(/\.json$/, '.min.json');
-    mkdirp(path.dirname(filePath), function(err) {
+    mkdirp(path.dirname(filePath), function (err) {
       if (err) {
         reject(err);
       } else {
-        fs.writeFileAsync(filePath, JSON.stringify(meta, null, 2))
-          .then(function(r) {
-            fs.writeFileAsync(minFilePath, JSON.stringify(meta))
-              .then(function(r2) {
+        var newData = JSON.stringify(meta, null, 2);
+        fs.readFileAsync(filePath, 'utf8').then(function (oldData) {
+          if (appJsonWrites.length > 0 && oldData !== newData) {
+            fs.writeFileAsync(filePath, newData, 'utf8').then(function (r) {
+              fs.writeFileAsync(minFilePath, JSON.stringify(meta), 'utf8').then(function (r2) {
                 fulfill([r, r2]);
-              })
-              .catch(reject);
-          })
-          .catch(reject);
+              }).catch(reject);
+            }).catch(reject);
+          } else {
+            fulfill([]);
+          }
+        }).catch(reject);
       }
     });
   });
