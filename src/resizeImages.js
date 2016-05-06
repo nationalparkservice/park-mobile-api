b5f68be82af08a8e3eff4098e7887eea7aa12887
Jim McAndrew <jim@loc8.us>
2016-05-06 14:35:31 -0600

    Better promises

--- src/resizeImages.js
+++ src/resizeImages.js
@@ -1,30 +1,26 @@
-var datawrap = require('datawrap'),
-  magickResizeWrapper = require('./magickResizeWrapper'),
-  path = require('path');
+var iterateTasks = require('./iterateTasks');
+var magickResizeWrapper = require('./magickResizeWrapper');
+var path = require('path');
 
-module.exports = function(options) {
-  var filePath = options.file,
-    fileTypes = options.fileTypes,
-    mediaDirectory = options.mediaDirectory.replace(/(\/$)/g,'') + '/',
-    uuid = options.uuid;
+module.exports = function (options) {
+  var filePath = options.file;
+  var fileTypes = options.fileTypes;
+  var mediaDirectory = options.mediaDirectory.replace(/(\/$)/g, '') + '/';
+  var uuid = options.uuid;
 
-  return new datawrap.Bluebird(function(fulfill, reject) {
-    var taskList = fileTypes.map(function(type, index) {
-      var outputFile = mediaDirectory + uuid + '_' + type + path.extname(filePath);
-      return {
-        'name': 'Resize to ' + type,
-        'task': magickResizeWrapper,
-        'params': [{
-          f: filePath,
-          o: outputFile,
-          t: type,
-          _deleteOriginal: index === fileTypes.length - 1 // Delete the image on the last task
-        }]
-      };
-    });
-
-    datawrap.runList(taskList)
-      .then(fulfill)
-      .catch(reject);
+  var taskList = fileTypes.map(function (type, index) {
+    var outputFile = mediaDirectory + uuid + '_' + type + path.extname(filePath);
+    return {
+      'name': 'Resize to ' + type,
+      'task': magickResizeWrapper,
+      'params': [{
+        f: filePath,
+        o: outputFile,
+        t: type,
+        _deleteOriginal: index === fileTypes.length - 1 // Delete the image on the last task
+      }]
+    };
   });
+
+  return iterateTasks(taskList);
 };
