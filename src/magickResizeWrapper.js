453b3dcb9d951f997e61ff8f7c59fbf23a89c830
Jim McAndrew <jim@loc8.us>
2016-05-06 14:33:29 -0600

    Better promises

--- src/magickResizeWrapper.js
+++ src/magickResizeWrapper.js
@@ -1,14 +1,14 @@
-var datawrap = require('datawrap'),
-  fs = require('fs'),
-  magickResize = require('magick-resize'),
-  path = require('path'),
-  mkdirp = require('mkdirp');
+var Promise = require('bluebird');
+var fs = require('fs');
+var magickResize = require('magick-resize');
+var mkdirp = require('mkdirp');
+var path = require('path');
 
-datawrap.Bluebird.promisifyAll(fs);
+Promise.promisifyAll(fs);
 
 // A wrapper to make magickResize easier to work with
-module.exports = function(args) {
-  return new datawrap.Bluebird(function(fulfill, reject) {
+module.exports = function (args) {
+  return new Promise(function (fulfill, reject) {
     // magickResize takes single letter args since it is a command line tool, so we just use the first letter
     var newArgs = {};
     for (var arg in args) {
@@ -16,21 +16,21 @@ module.exports = function(args) {
         newArgs[arg.substr(0, 1)] = args[arg];
       }
     }
-    mkdirp(path.dirname(newArgs.o), function(error) {
+    mkdirp(path.dirname(newArgs.o), function (error) {
       if (error) {
         reject(error);
       } else {
-        magickResize(newArgs, function(e, r) {
+        magickResize(newArgs, function (e, r) {
           if (e || !r) {
             reject(e.stack ? e : new Error(e));
           } else {
             if (args._deleteOriginal) {
-              fs.unlinkAsync(newArgs.f).then(function() {
+              fs.unlinkAsync(newArgs.f).then(function () {
                 fulfill({
                   result: r,
                   file: args.o
                 });
-              }).catch(function(e) {
+              }).catch(function (e) {
                 reject(e);
               });
             } else {
