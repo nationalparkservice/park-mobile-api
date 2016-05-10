7c3c7d0fe681ccc7d6708d92d60bbcaf1573aeec
Jim McAndrew <jim@loc8.us>
2016-05-10 09:57:24 -0600

    Clean up to semistandard: Update src/aggregationTools/getParkList.js

--- src/aggregationTools/getParkList.js
+++ src/aggregationTools/getParkList.js
@@ -1,28 +1,28 @@
 // Requires
-var datawrap = require('datawrap');
+var Promise = require('bluebird');
 var config = require('../../config');
+var datawrap = require('datawrap');
 var fs = require('fs');
 
 // Tools
 config.database.cartodb.apiKey = fs.readFileSync(config.database.cartodb.apiKey).toString();
 var database = datawrap(config.database.cartodb, config.database.defaults);
-var Bluebird = datawrap.Bluebird;
 
-module.exports = function(unitCode) {
-  return new Bluebird(function(fulfill, reject) {
-    var fieldName = 'unit_code',
-      parksQuery = 'SELECT DISTINCT {{fieldName}} FROM places_mobile_parks';
-    //fulfill(Array.isArray(unitCode) ? unitCode : [unitCode]);
+module.exports = function (unitCode) {
+  return new Promise(function (fulfill, reject) {
+    var fieldName = 'unit_code';
+    var parksQuery = 'SELECT DISTINCT {{fieldName}} FROM places_mobile_parks';
+    // fulfill(Array.isArray(unitCode) ? unitCode : [unitCode]);
     database.runQuery(parksQuery, {
-        fieldName: fieldName
-      })
-      .then(function(result) {
-        var availableUnitCodes = result[0].rows.map(function(row) {
+      fieldName: fieldName
+    })
+      .then(function (result) {
+        var availableUnitCodes = result[0].rows.map(function (row) {
           return row[fieldName];
         });
         if (unitCode) {
           unitCode = Array.isArray(unitCode) ? unitCode : [unitCode];
-          availableUnitCodes = availableUnitCodes.filter(function(uc) {
+          availableUnitCodes = availableUnitCodes.filter(function (uc) {
             return unitCode.indexOf(uc) >= 0;
           });
         }
