49c0ac67cd807d1a900aca38c00224e9d5fb5b81
Jim McAndrew <jim@loc8.us>
2017-04-04 16:20:01 -0600

    Update SSL and add database API

--- app.js
+++ app.js
@@ -2,16 +2,35 @@ var Api = require('./src/api');
 var allowXSS = require('./src/allowXSS');
 var config = require('./config');
 var express = require('express');
+var path = require('path');
+var DatabaseApi = require('./src/db/api');
+
+// Deal with the DOI issues
+require('ssl-root-cas').addFile('./secrets/DOIRootCA.crt');
+
 var port = config.port;
-var htmlDirectory = __dirname + '/' + config.htmlDirectory;
+var htmlDirectory = path.join(__dirname, config.htmlDirectory);
 
 var app = express();
 var api = new Api(htmlDirectory);
+var databaseApi = new DatabaseApi();
 var apiRouter = express.Router();
 allowXSS(apiRouter);
 
-api.map(function (apiCall) {
-  apiRouter[apiCall.method.toLowerCase()]('/api' + apiCall.path, apiCall.process);
+var addtoRouter = function (router, path, apiCall) {
+  var params = [];
+  params.push('/' + path + apiCall.path);
+  params = params.concat(apiCall.process);
+  console.log(params);
+  router[apiCall.method.toLowerCase()].apply(apiRouter, params);
+};
+
+api.forEach(function (apiCall) {
+  addtoRouter(apiRouter, 'api', apiCall);
+});
+
+databaseApi.forEach(function (apiCall) {
+  addtoRouter(apiRouter, 'db', apiCall);
 });
 
 app.use(apiRouter);
