2e0e7e06150039f19d2279233446e77c2d33e47b
Jim McAndrew <jim@loc8.us>
2017-04-04 16:20:38 -0600

    Use .any()

--- src/imageMulter.js
+++ src/imageMulter.js
@@ -2,7 +2,7 @@ var multer = require('multer');
 
 module.exports = multer({
   dest: './uploads/',
-  rename: function(fieldname, filename) {
+  rename: function (fieldname, filename) {
     return filename.replace(/\W+/g, ' - ').toLowerCase() + Date.now();
   }
-});
+}).any();
