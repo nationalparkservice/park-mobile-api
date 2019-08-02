20870290bf375edce72ea66919351ee89133f9d0
Jim McAndrew <jim@loc8.us>
2019-08-02 13:10:56 -0600

    Update app.js

--- api/app.js
+++ api/app.js
@@ -1,40 +1,17 @@
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+
 var Api = require('./src/api');
 var allowXSS = require('./src/allowXSS');
-var config = require('./config');
 var express = require('express');
-var path = require('path');
-var DatabaseApi = require('./src/db/api');
-var bodyParser = require('body-parser')
-
-
-// Deal with the DOI issues
-require('ssl-root-cas').addFile('./secrets/DOIRootCA.crt');
-
-var port = config.port;
-var htmlDirectory = path.join(__dirname, config.htmlDirectory);
+var port = process.env.API_PORT;
 
 var app = express();
-var api = new Api(htmlDirectory);
-var databaseApi = new DatabaseApi();
+var api = new Api();
 var apiRouter = express.Router();
-app.use(bodyParser.urlencoded({ extended: false }))
-
 allowXSS(apiRouter);
 
-var addtoRouter = function (router, path, apiCall) {
-  var params = [];
-  params.push('/' + path + apiCall.path);
-  params = params.concat(apiCall.process);
-  console.log(params);
-  router[apiCall.method.toLowerCase()].apply(apiRouter, params);
-};
-
-api.forEach(function (apiCall) {
-  addtoRouter(apiRouter, 'api', apiCall);
-});
-
-databaseApi.forEach(function (apiCall) {
-  addtoRouter(apiRouter, 'db', apiCall);
+api.map(function (apiCall) {
+  apiRouter[apiCall.method.toLowerCase()]('/api' + apiCall.path, apiCall.process);
 });
 
 app.use(apiRouter);
