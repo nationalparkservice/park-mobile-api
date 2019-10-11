64c4f896bf6afb9a4d5e1acfb6dce2800c765888
Jim McAndrew <jim@loc8.us>
2019-10-11 12:45:08 -0600

    Clean up formatting

--- api/app.js
+++ api/app.js
@@ -10,12 +10,12 @@ var api = new Api();
 var apiRouter = express.Router();
 allowXSS(apiRouter);
 
-api.map(function (apiCall) {
+api.map(function(apiCall) {
   apiRouter[apiCall.method.toLowerCase()]('/api' + apiCall.path, apiCall.process);
 });
 
 app.use(apiRouter);
 
-app.listen(port, function () {
+app.listen(port, function() {
   console.log('Working on port ' + port);
 });
