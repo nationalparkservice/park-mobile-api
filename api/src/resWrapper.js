f33750a35014a59adc64243c20937f3aec9ccf8e
Jim McAndrew <jim@loc8.us>
2019-10-17 10:00:45 -0600

    Move appSchemaJson into the config: Update api/src/resWrapper.js

--- api/src/resWrapper.js
+++ api/src/resWrapper.js
@@ -15,33 +15,57 @@ module.exports = function (req, origRes) {
         });
       },
       'send': function (result, options) {
-        var buf;
-        var newResult = typeof result === 'object' ? JSON.stringify(result, null, 2) : result.toString();
-        if (options.callback) {
-          newResult = 'function ' + options.callback + '(){return ' + newResult  + ';}';
-        }
-        var status = options && options.status || '200';
-        var returnValue;
-        if (!origRes.headersSent) {
-          origRes.status(status);
-          origRes.set({
-            'Content-Type': contentTypes[typeof result] ? contentTypes[typeof result] : contentTypes.default,
-            'X-Powered-By': 'NPMap',
-            'ETag': responseUuid
-          });
-          // If the request headers accept gzip, return it in gzip
-          if (req.headers['accept-encoding'] && req.headers['accept-encoding'].split(',').indexOf('gzip') >= 0) {
-            origRes.set('Content-Encoding', 'gzip');
-            buf = new Buffer(newResult, 'utf-8');
-            zlib.gzip(buf, function (err, zippedResult) {
-              returnValue = origRes.end(zippedResult);
-            });
-          } else {
-            returnValue = origRes.send(newResult);
-          }
-        }
-        return returnValue; // Return the original result, just in case there's a need for that
-      }
+       var buf, newResult = typeof result === 'object' ? JSON.stringify(result, null, 2) : result.toString(),
+         status = options && options.status || '200',
+         returnValue;
+       if (!origRes.headersSent) {
+         origRes.status(status);
+         origRes.set({
+           'Content-Type': contentTypes[typeof result] ? contentTypes[typeof result] : contentTypes.default,
+           'X-Powered-By': 'NPMap',
+           'ETag': responseUuid
+         });
+         // If the request headers accept gzip, return it in gzip
+         if (req.headers['accept-encoding'] && req.headers['accept-encoding'].split(',').indexOf('gzip') >= 0) {
+           origRes.set('Content-Encoding', 'gzip');
+           buf = Buffer.from(newResult, 'utf-8');
+           zlib.gzip(buf, function(err, zippedResult) {
+             returnValue = origRes.end(zippedResult);
+           });
+         } else {
+           returnValue = origRes.send(newResult);
+         }
+       }
+       return returnValue; // Return the original result, just in case there's a need for that
+     }
+      // {
+      //   var buf;
+      //   var newResult = typeof result === 'object' ? JSON.stringify(result, null, 2) : result.toString();
+      //   if (options.callback) {
+      //     newResult = 'function ' + options.callback + '(){return ' + newResult  + ';}';
+      //   }
+      //   var status = options && options.status || '200';
+      //   var returnValue;
+      //   if (!origRes.headersSent) {
+      //     origRes.status(status);
+      //     origRes.set({
+      //       'Content-Type': contentTypes[typeof result] ? contentTypes[typeof result] : contentTypes.default,
+      //       'X-Powered-By': 'NPMap',
+      //       'ETag': responseUuid
+      //     });
+      //     // If the request headers accept gzip, return it in gzip
+      //     if (req.headers['accept-encoding'] && req.headers['accept-encoding'].split(',').indexOf('gzip') >= 0) {
+      //       origRes.set('Content-Encoding', 'gzip');
+      //       buf = new Buffer(newResult, 'utf-8');
+      //       zlib.gzip(buf, function (err, zippedResult) {
+      //         returnValue = origRes.end(zippedResult);
+      //       });
+      //     } else {
+      //       returnValue = origRes.send(newResult);
+      //     }
+      //   }
+      //   return returnValue; // Return the original result, just in case there's a need for that
+      // }
     };
 
   // copy everything else over
