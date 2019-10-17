09bf1cd5e3af7c56fb800bf5609f4aff39dae018
Jim McAndrew <jim@loc8.us>
2019-10-17 10:00:45 -0600

    Move appSchemaJson into the config: Update api/src/aggregationTools/tests/aggregate.js

--- api/src/aggregationTools/tests/aggregate.js
+++ api/src/aggregationTools/tests/aggregate.js
@@ -1,7 +1,7 @@
 var aggregate = require('../../aggregate');
 var config = require('../../buildConfig')();
 
-aggregate('../../../app.schema.json', ['Brooklyn'] , config)
+aggregate(['Brooklyn'] , config)
   .then(function(r) {
     console.log(JSON.stringify(r, null, 2));
     console.log('Success!');
